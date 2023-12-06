2cdcbe1b39a707b833e3bc9699837f798b47286d
Jase Balderrama <jasebalderrama@icloud.com>
2023-12-05 21:02:19 -0800

    Print all 52 cards in deck

--- golang-the-complete-developers-guide/main.go
+++ golang-the-complete-developers-guide/main.go
@@ -1,12 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "2 of Spades")
-
+	cards := newDeck()
 	cards.printDeck()
-}
-
-func newCard() string {
-	return "5 of Diamonds"
 }
