4356c095691ba24af2c9411a427b18500c302db7
Jase Balderrama <jasebalderrama@icloud.com>
2023-12-10 17:58:45 -0800

    Create toString() & saveToFile() receiver funcs

--- golang-the-complete-developers-guide/main.go
+++ golang-the-complete-developers-guide/main.go
@@ -2,7 +2,8 @@ package main
 
 func main() {
 	cards := newDeck()
-	hand, remainingDeck := deal(cards, 5)
-	hand.printDeck()
-	remainingDeck.printDeck()
+	cards.saveToFile("52s")
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.printDeck()
+	// remainingDeck.printDeck()
 }
