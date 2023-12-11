7ce914bbc2b5ba39dfb873ddfcb88e942b2e9e60
Jase Balderrama <jasebalderrama@icloud.com>
2023-12-11 15:26:59 -0800

    Test deck for expected first & last card

--- golang-the-complete-developers-guide/deck_test.go
+++ golang-the-complete-developers-guide/deck_test.go
@@ -4,9 +4,19 @@ import "testing"
 
 func TestNewDeck(t *testing.T) {
 	expectedDeckLength := 52
+	expectedFirstCard := "Ace of Spades"
+	expectedLastCard := "Jack of Clubs"
 	d := newDeck()
 
 	if len(d) != expectedDeckLength {
 		t.Errorf("Expected deck length of %v, but got %v.", expectedDeckLength, len(d))
+	}
+
+	if d[0] != expectedFirstCard {
+		t.Errorf("Expected first card of '%v', but got '%v'.", expectedFirstCard, d[0])
+	}
+
+	if d[len(d)-1] != expectedLastCard {
+		t.Errorf("Expected last card of '%v', but got '%v'.", expectedLastCard, d[len(d)-1])
 	}
 }
