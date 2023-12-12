95380ede0f794e95358c0cde4987c3167ee07173
Jase Balderrama <jasebalderrama@icloud.com>
2023-12-11 17:35:10 -0800

    Finish Section 3

--- golang-the-complete-developers-guide/main.go
+++ golang-the-complete-developers-guide/main.go
@@ -2,8 +2,5 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.saveToFile("52s")
-	// hand, remainingDeck := deal(cards, 5)
-	// hand.printDeck()
-	// remainingDeck.printDeck()
+	cards.printDeck()
 }
